17684c264e64b6799109eb2252c6dbef6bd7b6a8
Traap <gary.a.howard@mac.com>
2020-12-31 19:25:27 -0600

    Separate personilization.

--- install.sh
+++ install.sh
@@ -194,29 +194,11 @@ if [[ $rbenvFlag == 1 ]]; then
 fi
 
 # -------------------------------------------------------------------------- }}}
-# {{{ Clone and build emend, when needed.
+# {{{ Personalize debian 
 
-if [[ $emendFlag == 1 ]]; then
-  mkdir -p $cloneRoot
-  cd $cloneRoot
-  git clone http://github.com/Traap/emend.git
-  cd emend
-
-  # Build and install emend
-  rake build:emend
-
-  # Clone and emend this system.
-  cd ..
-  git clone http://github.com/Traap/emend-computer.git
-
-  # Refresh bash shell."
-  source $HOME/.bashrc 
-
-  # Personalize Traap's environment.
-  # Note:  Only linux commands are ran when run from a wsl distro.
-  cd emend-computer
-  emend --verbose --nodryrun --bundle debian
-
+[[ $emendFlag == 1 ]] \
+  && ./personalize-debian.sh \
+  && echo "Personalization of debian.:
 fi
 
 # -------------------------------------------------------------------------- }}}
